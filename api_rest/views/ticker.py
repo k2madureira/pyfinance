47a6acb811aebb382af03a28c87fb79017c435dc
k2madureira <lenilsonmadureira@gmail.com>
2025-07-02 17:11:14 -0300

    feat: adicionado filtro por datas

--- api_rest/views/ticker.py
+++ api_rest/views/ticker.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from decimal import Decimal, InvalidOperation
 import requests
 import pandas as pd
 import io 
@@ -15,7 +16,7 @@ from rest_framework.pagination import PageNumberPagination
 
 from ..models import Ticker, Quote
 from ..serializers import TickerSerializer
-from ..utils import get_tickers_info, console_log
+from ..utils import get_tickers_info, get_sma_days, detecting_splits, atypical_volume, rsi, quotes_by_date, console_log, console_error
 
 
 class StandardResultsSetPagination(PageNumberPagination):
@@ -67,7 +68,45 @@ class TickersView(APIView):
 class TickerWithSymbolView(APIView):
     def get(self, request, symbol):
         try:
+            now_utc = timezone.now()       
+            yesterday_utc = now_utc - timedelta(days=1)          
+            yesterday = yesterday_utc.date()
 
+
+            sma_days = get_sma_days(request.query_params.get('sma_days'))
+            start_date = request.query_params.get('start_date')
+            if start_date:
+                start_date = datetime.strptime(request.query_params.get('start_date'), '%Y-%m-%d')
+            else:
+                start_date = (timezone.now() - timedelta(days=30))
+            
+
+            end_date = request.query_params.get('end_date')
+            if end_date:
+                end_date = datetime.strptime(request.query_params.get('end_date'), '%Y-%m-%d')
+            else:
+                end_date = datetime.now()
+
+            
+            rsi_days = request.query_params.get('rsi_days')
+            if rsi_days:
+                rsi_days = int(rsi_days)
+            else:
+                rsi_days = 14
+
+            sma_days_volume = request.query_params.get('sma_days_volume')
+            if sma_days_volume:
+                sma_days_volume = int(sma_days_volume)
+            else:
+                sma_days_volume = 10
+
+            
+            threshold= request.query_params.get('threshold')
+            if threshold:
+                threshold = float(threshold)
+            else:
+                threshold = 1.5
+          
             ticker = Ticker.objects.prefetch_related('quotes').get(symbol=symbol)
             serialized = TickerSerializer(ticker).data
 
@@ -87,11 +126,6 @@ class TickerWithSymbolView(APIView):
             
                         quote_date = datetime.fromisoformat(quote_date_str.replace('Z', '+00:00')).date()
                         quote_created_at = datetime.fromisoformat(quote_created_at_str.replace('Z', '+00:00')).date()
-                      
-                        now_utc = timezone.now()       
-                        yesterday_utc = now_utc - timedelta(days=1)          
-                        yesterday = yesterday_utc.date()
-
                    
                         if quote_date < yesterday or quote_created_at < yesterday: 
                             needs_api_fetch = True
@@ -107,8 +141,32 @@ class TickerWithSymbolView(APIView):
                 get_tickers_info([symbol])
                 ticker = Ticker.objects.prefetch_related('quotes').get(symbol=symbol)
                 serialized = TickerSerializer(ticker).data
-              
+
+
+            quotes = serialized.get('quotes', [])
+            quotes = quotes_by_date(quotes, start_date, end_date)
+
+            print(quotes)
+            quotes_sma = quotes[:sma_days] 
+            quote_prices = []
+            for q in quotes_sma:
+                if 'close_price' in q and q['close_price'] is not None:
+                    try:
+                        quote_prices.append(Decimal(q['close_price']))
+                    except InvalidOperation:
+                        console_error('ERROR', f"'invalid close_price': {q['close_price']}")
+        
+            if quote_prices:
+                sma_calculada = sum(quote_prices) / len(quote_prices)
                 
+            rsi_quotes = quotes
+            serialized['sma_days'] = sma_days
+            serialized['sma'] = f'{sma_calculada:.2f}'
+            serialized['splits'] = detecting_splits(quotes)
+            serialized['quotes']= quotes
+            serialized['atipcal_volumes'] = atypical_volume(rsi_quotes, sma_days_volume, threshold)
+            serialized['rsi'] = rsi(quotes[:rsi_days+14], rsi_days)
+
             return Response(serialized)
         
         except Ticker.DoesNotExist:
